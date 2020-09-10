9fffd52fe1ded1f46924a24aaa425cdd57bde1c6
Adam Olech <nddr89@gmail.com>
2020-09-10 20:31:07 +0200

    amixer.py: exceptions and clean-up

--- sixsixutil/amixer.py
+++ sixsixutil/amixer.py
@@ -1,6 +1,17 @@
 import subprocess
 from collections import namedtuple
 
+class AmixerException(Exception):
+    def __init__(self, message):
+        self.message = message.replace("\n", "")
+        super().__init__(self.message)
+
+class ControlNotFound(AmixerException):
+    pass
+
+class CardNotFound(AmixerException):
+    pass
+
 class Amixer:
     def __init__(self, card_id):
         self.card_id = card_id
@@ -35,12 +46,23 @@ class Amixer:
 
         return namedtuple("AmixerResult", d.keys())(*d.values())
 
-    def cget(self, control):
-        cmd = f"amixer -c {self.card_id} -M cget iface=MIXER,name='{control}'"
+    def __run_amixer(self, params):
+        cmd = f"amixer {params}"
         s = subprocess.run(cmd, shell=True, capture_output=True)
 
         if s.returncode != 0:
-            # Throw exception!!!
-            pass
+            stderr = s.stderr.decode()
+            if "Cannot find the given element from control" in stderr:
+                raise ControlNotFound(stderr)
+            elif "Invalid card number" in stderr:
+                raise CardNotFound(stderr)
 
         return self.__parse_amixer_stdout(s.stdout)
+
+    def cget(self, control):
+        cmd = f"-c {self.card_id} -M cget iface=MIXER,name='{control}'"
+        return self.__run_amixer(cmd)
+
+    def cset(self, control, left, right):
+        cmd = f"-c {self.card_id} cset iface=MIXER,name='{control}' {left},{right}"
+        return self.__run_amixer(cmd)
